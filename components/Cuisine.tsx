1854d64ea883a6c981f2771e1c18fa9d84483ef2
TypingWithBeako <thaituan10150806@gmail.com>
2025-08-05 18:04:08 +0700

    feat: Added main functionality to Cuisine section and removed scrollbar for immersiveness

--- components/Cuisine.tsx
+++ components/Cuisine.tsx
@@ -1,3 +1,26 @@
 export default function Cuisine() {
-  return <div>Hello world!</div>;
+  return (
+    <section id="Cuisine" className="bg-white">
+      <div className="container flex flex-col justify-center pt-20">
+        <h1 className="self-center text-3xl">Khám phá Ẩm thực của Huế!</h1>
+        <p className="text-xl py-8 self-center">
+          Hãy cùng tôi đi một vòng Huế, không phải để thăm các cung điện hay
+          lăng tẩm, mà để khám phá hương vị đặc trưng, để mỗi món ăn không chỉ
+          là một bữa tiệc vị giác, mà còn là một câu chuyện về văn hóa và con
+          người nơi đây.,
+        </p>
+      </div>
+      <div className="container h-[90vh] ">
+        <iframe
+          src="/story/index.html"
+          style={{
+            width: "100%",
+            height: "100%",
+            border: "none",
+          }}
+          title="Hue Food Story"
+        />
+      </div>
+    </section>
+  );
 }
